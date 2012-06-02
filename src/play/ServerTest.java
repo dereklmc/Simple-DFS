0b494400bd1735bc389591f1cce48a9a2085d8ae
Derek McLean <derek.l.mclean@gmail.com>
2012-06-01 17:25:19 -0700

    Added state to test server

--- src/play/ServerTest.java
+++ src/play/ServerTest.java
@@ -3,24 +3,69 @@ package play;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import server.DFSServer;
+
+import server.CachedFile.CacheState;
 
 import lib.FileContents;
 import lib.ServerInterface;
 
 public class ServerTest extends UnicastRemoteObject implements ServerInterface {
-
+	
+	public enum CacheState {
+		READ_SHARED, WRITE_SHARED, OWNERSHIP_CHANGED, NOT_SHARED;
+	}
+	
+	private CacheState state;
+	
 	public ServerTest() throws RemoteException {
 		super();
-		// TODO Auto-generated constructor stub
+		state = CacheState.NOT_SHARED;
+		printState();
+	}
+
+	private void printState() {
+		System.out.println(String.format("Current State: <%s>", state.name()));
 	}
 
 	@Override
 	public FileContents download(String client, String filename, String mode)
 			throws RemoteException {
 		System.out.println(String.format(
-				"Recieved Download request from %s for file \"%s\".", client,
-				filename));
+				"Recieved Download request from %s for file \"%s\" in mode <%s>.", client,
+				filename, mode));
+		printState();
+		System.out.println("Changing state...");
+		switch (state) {
+		case NOT_SHARED:
+			if (mode.equals("r")) {
+				state = CacheState.READ_SHARED;
+			} else {
+				state = CacheState.WRITE_SHARED;
+			}
+			break;
+		case READ_SHARED:
+			if (mode.equals("r")) {
+			} else {
+				state = CacheState.WRITE_SHARED;
+			}
+			break;
+		case WRITE_SHARED:
+			if (mode.equals("r")) {
+			} else {
+				state = CacheState.OWNERSHIP_CHANGED;
+			}
+			break;
+		case OWNERSHIP_CHANGED:
+			if (mode.equals("r")) {
+			} else {
+				// TODO
+			}
+			break;
+
+		default:
+			break;
+		}
+		printState();
 		return null;
 	}
 
