be97a9bf65a49a4f63e208d544d92091c9bb9641
Derek McLean <derek.l.mclean@gmail.com>
2012-06-02 23:12:51 -0700

    Trying something like Maurice's suggestion.

--- src/play/ServerTest.java
+++ src/play/ServerTest.java
@@ -29,8 +29,7 @@ public class ServerTest extends UnicastRemoteObject implements ServerInterface {
 	private String owner = "";
 	private String port;
 
-	public ServerTest(String port) throws RemoteException,
-			MalformedURLException {
+	public ServerTest(String port) throws RemoteException, MalformedURLException {
 		super();
 		readers = new HashSet<String>();
 		state = CacheState.NOT_SHARED;
@@ -46,69 +45,105 @@ public class ServerTest extends UnicastRemoteObject implements ServerInterface {
 	@Override
 	public FileContents download(String client, String filename, String mode)
 			throws RemoteException {
-		System.out
-				.println(String
-						.format("Recieved Download request from %s for file \"%s\" in mode <%s>.",
-								client, filename, mode));
+		System.out.println(String.format(
+				"Recieved Download request from %s for file \"%s\" in mode <%s>.", client,
+				filename, mode));
 		printState();
 		System.out.println("Changing state...");
-		do {
-			switch (state) {
-			case NOT_SHARED:
-				if (mode.equals("r")) {
-					readers.add(client);
-					state = CacheState.READ_SHARED;
-				} else {
-					owner = client;
-					state = CacheState.WRITE_SHARED;
-				}
-				break;
-			case READ_SHARED:
-				if (mode.equals("r")) {
-					readers.add(client);
-				} else {
-					owner = client;
-					state = CacheState.WRITE_SHARED;
-				}
-				break;
-			case OWNERSHIP_CHANGED:
-				if (mode.equals("r")) {
-					readers.add(client);
-					break;
-				} else {
-					// TODO
-					synchronized (this) {
+		if (mode.equals("r")) {
+			readers.add(client);
+			if (state == CacheState.NOT_SHARED)
+				state = CacheState.READ_SHARED;
+		} else {
+			if (state == CacheState.NOT_SHARED || state == CacheState.READ_SHARED) {
+				owner = client;
+				state = CacheState.WRITE_SHARED;
+			} else if (!client.equals(owner)) {
+				synchronized (this) {
+					while (state == CacheState.WRITE_SHARED) {
+						writebackClient(owner);
 						try {
-							while (state == CacheState.OWNERSHIP_CHANGED) {
-								wait();
-							}
+							wait();
 						} catch (InterruptedException e) {
-							RemoteException re = new RemoteException();
-							re.initCause(e);
-							throw re;
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+							break;
 						}
 					}
+					owner = client;
+					state = CacheState.WRITE_SHARED;
 				}
-			case WRITE_SHARED:
-				if (mode.equals("r")) {
-					readers.add(client);
-				} else {
-					state = CacheState.OWNERSHIP_CHANGED;
-					writebackClient(owner);
-				}
-				break;
-
-			default:
-				break;
 			}
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				RemoteException re = new RemoteException();
-				re.initCause(e);
-				throw re;
-			}
-		} while (state == CacheState.OWNERSHIP_CHANGED);
+		}
+		
+//		do {
+//			switch (state) {
+//			case NOT_SHARED:
+//				if (mode.equals("r")) {
+//					readers.add(client);
+//					state = CacheState.READ_SHARED;
+//				} else {
+//					owner = client;
+//					state = CacheState.WRITE_SHARED;
+//				}
+//				break;
+//			case READ_SHARED:
+//				if (mode.equals("r")) {
+//					readers.add(client);
+//				} else {
+//					owner = client;
+//					state = CacheState.WRITE_SHARED;
+//				}
+//				break;
+//			case OWNERSHIP_CHANGED:
+//				if (mode.equals("r")) {
+//					readers.add(client);
+//					break;
+//				} else {
+//					// TODO
+//					synchronized (this) {
+//						try {
+//							while (state == CacheState.OWNERSHIP_CHANGED) {
+//								wait();
+//							}
+//						} catch (InterruptedException e) {
+//							RemoteException re = new RemoteException();
+//							re.initCause(e);
+//							throw re;
+//						}
+//					}
+//				}
+//			case WRITE_SHARED:
+//				if (mode.equals("r")) {
+//					readers.add(client);
+//				} else {
+//					state = CacheState.OWNERSHIP_CHANGED;
+//					writebackClient(owner);
+//					synchronized (this) {
+//						try {
+//							while (state == CacheState.OWNERSHIP_CHANGED) {
+//								wait();
+//							}
+//						} catch (InterruptedException e) {
+//							RemoteException re = new RemoteException();
+//							re.initCause(e);
+//							throw re;
+//						}
+//					}
+//				}
+//				break;
+//
+//			default:
+//				break;
+//			}
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				RemoteException re = new RemoteException();
+//				re.initCause(e);
+//				throw re;
+//			}
+//		} while (state == CacheState.OWNERSHIP_CHANGED);
 		System.out.println("Current Readers:");
 		for (String reader : readers) {
 			System.out.println("\t" + reader);
@@ -149,21 +184,18 @@ public class ServerTest extends UnicastRemoteObject implements ServerInterface {
 		}
 	}
 
-	public ClientInterface getRemoteClient(String clientAddress)
-			throws NotBoundException, MalformedURLException, RemoteException {
-		String rmiClientAddress = String.format("rmi://%s:%s/fileclient",
-				clientAddress, port);
-		ClientInterface client = (ClientInterface) Naming
-				.lookup(rmiClientAddress);
+	public ClientInterface getRemoteClient(String clientAddress) throws NotBoundException,
+			MalformedURLException, RemoteException {
+		String rmiClientAddress = String.format("rmi://%s:%s/fileclient", clientAddress, port);
+		ClientInterface client = (ClientInterface) Naming.lookup(rmiClientAddress);
 		return client;
 	}
 
 	@Override
 	public boolean upload(String client, String filename, FileContents contents)
 			throws RemoteException {
-		System.out.println(String.format(
-				"Recieved Upload request from %s for file \"%s\".", client,
-				filename));
+		System.out.println(String.format("Recieved Upload request from %s for file \"%s\".",
+				client, filename));
 		printState();
 		for (Iterator<String> iterator = readers.iterator(); iterator.hasNext();) {
 			String reader = iterator.next();
@@ -171,19 +203,23 @@ public class ServerTest extends UnicastRemoteObject implements ServerInterface {
 			invalidateClient(reader);
 			iterator.remove();
 		}
-		switch (state) {
-		case WRITE_SHARED:
-			state = CacheState.NOT_SHARED;
-			break;
-		case OWNERSHIP_CHANGED:
-			state = CacheState.WRITE_SHARED;
-			synchronized (this) {
-				notifyAll();
-			}
-			break;
-		default:
-			break;
+		state = CacheState.NOT_SHARED;
+		synchronized (this) {
+			notifyAll();
 		}
+//		switch (state) {
+//		case WRITE_SHARED:
+//			state = CacheState.NOT_SHARED;
+//			break;
+//		case OWNERSHIP_CHANGED:
+//			state = CacheState.WRITE_SHARED;
+//			synchronized (this) {
+//				notifyAll();
+//			}
+//			break;
+//		default:
+//			break;
+//		}
 		printState();
 		return false;
 	}
