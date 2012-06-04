0ca1849e6e763499a52991174c9ae09a31a3feb9
Derek McLean <derek.l.mclean@gmail.com>
2012-06-03 17:28:23 -0700

    Created a proxy for interacting with clients.

---
+++ src/server/ClientProxy.java
@@ -0,0 +1,46 @@
+package server;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import lib.ClientInterface;
+
+/**
+ * Establishes a connection to a remote client object.
+ * @author derek
+ *
+ */
+public class ClientProxy implements ClientInterface {
+	
+	private String name;
+	private ClientInterface client;
+
+	public ClientProxy(String address, String port) throws MalformedURLException, RemoteException, NotBoundException {
+		name = address;
+		
+		String rmiClientAddress = String.format("rmi://%s:%s/fileclient", address, port);
+		client = (ClientInterface) Naming.lookup(rmiClientAddress);
+	}
+	
+	public ClientProxy(String name, ClientInterface client) {
+		this.name = name;
+		this.client = client;
+	}
+
+	@Override
+	public boolean invalidate() throws RemoteException {
+		return client.invalidate();
+	}
+
+	@Override
+	public boolean writeback() throws RemoteException {
+		return client.writeback();
+	}
+
+	public String getClientName() {
+		return name;
+	}
+
+}
