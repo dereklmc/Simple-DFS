c35ae72286ded26005ee462113a168a0aaddfc33
Derek McLean <dlm18@uw.edu>
2012-06-06 17:10:04 -0700

    Removed class files

--- bin/runClient.sh
+++
@@ -1,5 +0,0 @@
-clear
-rmiregistry $DLMPORT &
-RMIREG_PID=$!
-java DFSClient $UW21 $DLMPORT
-kill -9 $RMIREG_PID
--- bin/runServer.sh
+++
@@ -1,5 +0,0 @@
-clear
-rmiregistry $DLMPORT &
-RMIREG_PID=$!
-java DFSServer $DLMPORT
-kill -9 $RMIREG_PID
